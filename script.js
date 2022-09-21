6f07012f52d5267ef62eb8719b5ea7493dbae23b
TeefAlfadhli <105872717+TeefAlfadhli@users.noreply.github.com>
2022-09-21 21:02:16 +0300

    last HW is done

--- script.js
+++ script.js
@@ -7,31 +7,45 @@ let random_grade_div = document.getElementById("random-grade");
 
 
 /*  الجزء الأول والثاني من التمرين */
+grades.pop()
+console.log(grades)
 
+grades.push = 99;
+console.log(grades)
 
+grades.forEach((grades) => (all_grades_div.innerHTML += `${grades}`+ " "));
 
-
+let randomIndex = Math.floor(Math.random()*grades.length);
+// console.log(randomIndex)
+grades.forEach((grades) => (random_grade_div.innerHTML += `${randomIndex}: ${grades[randomIndex]}`));
 
 
 
 
 
 /* الجزء الثالث من التمرين */
+//Change studentsCount,track property here
 
 let course = {
   name: "UniCODE",
-  track : "",
-  studentsCount : 0,
+  track : "web",
+  studentsCount : 40,
   location : "Kuwait University"
 }
 
-//Change studentsCount,track property here
+
 
 
 
 //Console log here
+console.log(document.getElementById("MyDiv").innerText += "Hello my name is taif " +"i'm in "+ course.track+ " at "+course.name);
+
 
 
 
 //Create student object here
-
+let student = 
+{
+  Name:"Taif",
+  university: "AUM"
+}
